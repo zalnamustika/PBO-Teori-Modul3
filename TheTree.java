2c475082d3a6759352fff0031443f5c113cde373
Zalna Mustika <109508971+zalnamustika11@users.noreply.github.com>
2022-09-24 02:28:11 +0700

    file tugas modul 3

---
+++ TheTree.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modul2;
+
+/**
+ *
+ * @author DELL
+ */
+public class TheTree {
+    public static void main (String[] arg){
+        System.out.println("I think I shall never see,");
+        System.out.println("a poem lovely as a tree.");
+        System.out.println("A tree whose hungry mouth is pressed");
+        System.out.println("Against the Earth's sweet flowing breast.");
+    }
+}
